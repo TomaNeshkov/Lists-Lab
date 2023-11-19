b8e326472cceca9fc94f68d0beab53153dfed065
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-19 15:35:42 +0200

    Exercises (100/100)

--- 4. List of Products/4. List of Products/Program.cs
+++ 4. List of Products/4. List of Products/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        List<string> products = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string currentProduct = Console.ReadLine();
+            products.Add(currentProduct);
+        }
+
+        products.Sort();
+
+        for (int i = 0; i < products.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.{products[i]}");
+        }
+    }
+}
