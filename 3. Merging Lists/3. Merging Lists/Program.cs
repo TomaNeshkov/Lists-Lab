c7e79554b7747da1737b024ac2f6cc9f2b04704b
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-19 15:31:24 +0200

    Exercises (100/100)

--- 3. Merging Lists/3. Merging Lists/Program.cs
+++ 3. Merging Lists/3. Merging Lists/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> firstList = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        List<int> secondList = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        List<int> mergedList = MergeLists(firstList, secondList);
+
+        Console.WriteLine(string.Join(" ", mergedList));
+    }
+
+    static List<int> MergeLists(List<int> firstList, List<int> secondList)
+    {
+        List<int> result = new List<int>();
+        int minLength = Math.Min(firstList.Count, secondList.Count);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            result.Add(firstList[i]);
+            result.Add(secondList[i]);
+        }
+
+        result.AddRange(firstList.Skip(minLength));
+        result.AddRange(secondList.Skip(minLength));
+
+        return result;
+    }
+}
