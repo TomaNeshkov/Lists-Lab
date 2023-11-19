5761fba13cc462e56641531fd02577c6525b9e28
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-19 15:27:14 +0200

    Exercises (100/100)

--- 2. Gauss' Trick/2. Gauss' Trick/Program.cs
+++ 2. Gauss' Trick/2. Gauss' Trick/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        SumGaussTrick(numbers);
+
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+
+    static void SumGaussTrick(List<int> numbers)
+    {
+        int originalCount = numbers.Count;
+
+        for (int i = 0; i < originalCount / 2; i++)
+        {
+            numbers[i] += numbers[numbers.Count - 1];
+            numbers.RemoveAt(numbers.Count - 1);
+        }
+    }
+}
