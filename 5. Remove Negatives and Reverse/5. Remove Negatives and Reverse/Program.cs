7ec50057c96d20b1bb36b9a87cfe8a60afc167f9
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-11-19 20:57:04 +0200

    Exercises

--- 5. Remove Negatives and Reverse/5. Remove Negatives and Reverse/Program.cs
+++ 5. Remove Negatives and Reverse/5. Remove Negatives and Reverse/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        RemoveNegativesAndReverse(numbers);
+    }
+
+    static void RemoveNegativesAndReverse(List<int> numbers)
+    {
+        numbers.RemoveAll(num => num < 0);
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ 7. List Manipulation Advanced/7. List Manipulation Advanced/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> distances = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int sumOfRemovedElements = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != null && input != "")
+            {
+                int index = int.Parse(input);
+
+                if (index < 0)
+                {
+                    int removedElement = distances[0];
+                    sumOfRemovedElements += removedElement;
+                    distances.RemoveAt(0);
+                    distances.Insert(0, distances[distances.Count - 1]);
+
+                    IncreaseDecrease(distances, removedElement);
+                }
+                else if (index >= distances.Count)
+                {
+                    int removedElement = distances[distances.Count - 1];
+                    sumOfRemovedElements += removedElement;
+                    distances.RemoveAt(distances.Count - 1);
+                    distances.Add(distances[0]);
+
+                    IncreaseDecrease(distances, removedElement);
+                }
+                else
+                {
+                    int removedElement = distances[index];
+                    sumOfRemovedElements += removedElement;
+                    distances.RemoveAt(index);
+
+                    IncreaseDecrease(distances, removedElement);
+                }
+            }
+
+            Console.WriteLine(sumOfRemovedElements);
+        }
+
+        static void IncreaseDecrease(List<int> distances, int removedElement)
+        {
+            for (int i = 0; i < distances.Count; i++)
+            {
+                if (distances[i] <= removedElement)
+                {
+                    distances[i] += removedElement;
+                }
+                else
+                {
+                    distances[i] -= removedElement;
+            }
+        }
+    }
+}
